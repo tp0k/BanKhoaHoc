2f13ec3e91341f87db682b7872a7323b56856f17
Tạ Thị Phương Thảo <93700327+tp0k@users.noreply.github.com>
2024-04-26 01:35:26 +0700

    tạo các middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,9 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'checkrole' => \App\Http\Middleware\checkRole::class,
+        'checkauth' => \App\Http\Middleware\checkAuth::class,
+        'checkstudent' => \App\Http\Middleware\checkStudent::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
