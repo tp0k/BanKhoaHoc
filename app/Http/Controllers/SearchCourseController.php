dd7f669160ef2d2cabae485b3074880189360a03
lan <tothituyetlanvbhp@gmail.com>
2024-05-23 00:08:56 +0700

    sửa tìm kiếm trạng thái

--- app/Http/Controllers/SearchCourseController.php
+++ app/Http/Controllers/SearchCourseController.php
@@ -10,32 +10,27 @@ class SearchCourseController extends Controller
 {
     public function index(Request $request)
     {
-        
         $category = CourseCategory::get();
         $selectedCategories = $request->input('categories', []);
-#mới sửa
-        // $course = Course::where('status', 2)->when($selectedCategories, function ($query) use ($selectedCategories) {
-        //         $query->whereIn('course_category_id', $selectedCategories);
-        //     })->get();
-        
         $keywordf = $request->input('keywordf', '');
         $course = Course::where('status', 2)
         ->when($selectedCategories, function ($query) use ($selectedCategories) {
             $query->whereIn('course_category_id', $selectedCategories);
         })
         ->when($keywordf, function ($query) use ($keywordf) {
-            $query->where('title_en', 'like', "%$keywordf%")
+            $query->Where(function ($subquery) use ($keywordf) { 
+            $subquery->where('status', 2)
+                ->where('title_en', 'like', "%$keywordf%")
                 ->orWhere('description_en', 'like', "%$keywordf%")
                 ->orWhereHas('instructor', function ($subquery) use ($keywordf) {
                     $subquery->where('name_en', 'like', "%$keywordf%");
                 });
-        })
+        });
+    })
         ->get();
-#
-            
-
         $allCourse = Course::where('status', 2)->get();
 
         return view('frontend.searchCourse', compact('course', 'category', 'selectedCategories', 'allCourse'));
     }
+
 }
