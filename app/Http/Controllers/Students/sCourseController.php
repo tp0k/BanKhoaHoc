79a13c5461a753928a1b26e339c134cbbadf50a4
Tạ Thị Phương Thảo <93700327+tp0k@users.noreply.github.com>
2024-05-31 22:54:39 +0700

    khoá học liên quan

--- app/Http/Controllers/Students/sCourseController.php
+++ app/Http/Controllers/Students/sCourseController.php
@@ -13,25 +13,28 @@ class sCourseController extends Controller
     public function Show(Request $request, $id)
     {
         $course = Course::findOrFail(encryptor('decrypt', $id));
-        $relatedCourses = $this->relatedCourses($course->course_category_id);// Lấy danh sách khoá học liên quan
         $user_id = currentUserId();
         // dd($user_id);
         // Kiểm tra xem học sinh đã đăng ký khoá học hay chưa
         $enrollment = Enrollment::where('student_id', $user_id)
                                 ->where('course_id', $course->id)
                                 ->first();
-        
-        return view('frontend.courseDetails', compact('course', 'enrollment', 'relatedCourses'));
+        // Lấy ID của category của khóa học hiện tại
+        $courseCategoryId = $course->course_category_id;
+
+        // Lấy danh sách các khoá học liên quan dựa trên category của khóa học hiện tại và ID của khóa học hiện tại
+        $relatedCourses = $this->relatedCourses($courseCategoryId, $course->id);
+        return view('frontend.courseDetails', compact('course', 'enrollment', 'relatedCourses', 'courseCategoryId'));
     }
 
-    public function relatedCourses($courseCategoryId)
+    public function relatedCourses($courseCategoryId, $currentCourseId)
     {
         // Lấy danh sách các khoá học liên quan dựa trên course_category_id của khóa học đang hiển thị
         $relatedCourses = Course::where('course_category_id', $courseCategoryId)
-                                // ->where('id', '!=', $this->course->id) // Loại trừ khoá học đang hiển thị
-                                ->limit(5) // Giới hạn số lượng khoá học liên quan
+                                ->where('id', '!=', $currentCourseId) // Loại trừ khóa học đang hiển thị
+                                // ->limit(5) // Giới hạn số lượng khóa học liên quan
                                 ->get();
-        // dd($relatedCourses);
+
         return $relatedCourses;
     }
 }
