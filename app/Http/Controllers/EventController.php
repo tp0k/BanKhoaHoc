8889eb967f950145bbcd2a1888b4654509c23790
Dan2512 <164901649+Dan2512@users.noreply.github.com>
2024-06-05 19:44:25 +0700

    fix nút sửa, xóa của banners

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -13,18 +13,11 @@ class EventController extends Controller
         $event = Event::get();
         return view('backend.event.index', compact('event'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('backend.event.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         try {
@@ -32,10 +25,7 @@ class EventController extends Controller
             $event->title = $request->title;
             $event->description = $request->description;
             $event->content = $request->content;
-            // $event->topic = $request->topic;
-            // $event->goal = $request->goal;
-            // $event->hosted_by = $request->hosted_by;
-            // $event->date = $request->date;
+
             if ($request->hasFile('image')) {
                 $imageName = rand(999, 111) . time() . '.' . $request->image->extension();
                 $request->image->move(public_path('uploads/events'), $imageName);
@@ -55,25 +45,18 @@ class EventController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Event $event)
     {
         //
     }
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit($id)
     {
         $event = Event::findOrFail($id);
         return view('backend.event.edit', compact('event'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, $id)
     {
         try {
@@ -81,10 +64,6 @@ class EventController extends Controller
             $event->title = $request->title;
             $event->description = $request->description;
             $event->content = $request->content;
-            // $event->topic = $request->topic;
-            // $event->goal = $request->goal;
-            // $event->hosted_by = $request->hosted_by;
-            // $event->date = $request->date;
             if ($request->hasFile('image')) {
                 $imageName = rand(999, 111) . time() . '.' . $request->image->extension();
                 $request->image->move(public_path('uploads/events'), $imageName);
@@ -103,10 +82,6 @@ class EventController extends Controller
             return redirect()->back()->withInput();
         }
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy($id)
     {
         $data = Event::findOrFail($id);
