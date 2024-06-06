76597ea77bccfe51408d7d087891bfda718069a4
lan <tothituyetlanvbhp@gmail.com>
2024-06-06 23:45:54 +0700

    xóa bảng checkouts, discussions, messages, options, orders, payments, progress, subscription

---
+++ database/migrations/2024_06_06_161733_delete_messages_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     *  @return void
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+
+    /**
+     *  @return void
+     */
+    public function down(): void
+    {
+        //
+    }
+};
